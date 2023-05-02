438246b0515a2ddd22cc314800022db0d5895f39
Mshah2003 <123729708+Mshah2003@users.noreply.github.com>
2023-05-02 09:18:27 +0530

    Update Whatsapp_message.py

--- Whatsapp_message.py
+++ Whatsapp_message.py
@@ -1,11 +1,21 @@
 import pywhatkit
 
+
+# Line 6 to 10 for personal number
+
 number = input("Enter phone number with country code")
 message = input("Enter message to be delivered")
 time_hrs = int(input("Enter hours (in 24hr format)"))
 time_mins = int(input("Enter mins"))
 pywhatkit.sendwhatmsg(number, message, time_hrs, time_mins, 15, True, 3)
 
+# Line 14 to 18 for groups
 
-group_ID = input()
-pywhatkit.sendwhatmsg_to_group_instantly(group_ID, message, 15, True, 3)
+group_ID = input("Enter group ID")
+message = input("Enter message to be delivered")
+time_hrs = int(input("Enter hours (in 24hr format)"))
+time_mins = int(input("Enter mins"))
+pywhatkit.sendwhatmsg_to_group(group_ID, message, time_hrs, time_mins, 15, True, 3)
+
+
+# comment out the one which is not being used i.e. any one of group or personal number
